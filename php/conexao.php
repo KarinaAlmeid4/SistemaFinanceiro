df6f65386b7895146c6209061ef882bd841bb45d
Karina Almeida <Karina.Almeida161@outlook.com>
2024-09-30 21:32:37 -0300

    Site

---
+++ php/conexao.php
@@ -0,0 +1,19 @@
+<?php
+//Informações para conectar no banco de dados
+
+$servidor = 'localhost';
+$usuario = 'root';
+$db = 'db_contabilidade';
+$senha = 'root';
+
+$conexao = new mysqli($servidor,$usuario,$senha,$db) ;
+
+if ($conexao ->connect_error) {
+    die ('Falha na Conexão' . $conexao->connect_error);
+}else{
+    echo "Conectado com sucesso";
+}
+
+
+
+?>
